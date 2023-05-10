13b9d589d94c518925ad130b833c085f4511ba4d
Sitardaniel <sity143@gmail.com>
2023-05-10 23:01:00 +0000

    update

--- e2e.py
+++ e2e.py
@@ -1,34 +1,52 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+import os
 
 def test_scores_service(url):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run in headless mode, no visible browser window
-    service = Service('path_to_chromedriver')  # Replace with the path to your chromedriver executable
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    # Set Firefox options
+    options = Options()
+    options.headless = True  # Run Firefox in headless mode (without opening a visible browser window)
 
+    # Set the path to the geckodriver executable
+    geckodriver_path = '/usr/bin/geckodriver'
+
+    # Create a Firefox WebDriver instance
+    driver = webdriver.Firefox(options=options, executable_path=geckodriver_path)
+
+    # Navigate to the provided URL
     driver.get(url)
 
-    score_element = driver.find_element(By.CSS_SELECTOR, "selector_for_score_element")
-    score_text = score_element.text
-
     try:
+        # Find the score element on the web page
+        score_element = driver.find_element(By.ID, "score")
+
+        # Get the text of the score element
+        score_text = score_element.text
+
+        # Convert the score to an integer
         score = int(score_text)
+
+        # Check if the score is between 1 and 1000
         if 1 <= score <= 1000:
             return True
         else:
             return False
-    except ValueError:
+
+    except Exception as e:
+        print("Error occurred during testing:", str(e))
         return False
 
-    driver.quit()
+    finally:
+        # Quit the browser
+        driver.quit()
 
 def main_function():
-    url = input("Enter the URL: ")
+    # Call the test_scores_service function with the application URL
+    url = "http://127.0.0.1:5000"
+    result = test_scores_service(url)
 
-    if test_scores_service(url):
+    if result:
         print("Tests passed!")
         return 0
     else:
@@ -38,3 +56,4 @@ def main_function():
 if __name__ == "__main__":
     exit_code = main_function()
     exit(exit_code)
+
